be4c8b4a1ef3962656814f6f36f6183bf9d4e905
Erp <erp@163.com>
2024-07-09 11:26:51 +0800

    leedCode算法题:两数之和、三数之和

---
+++ src/main/java/com/warship/leedcode/shuzu/_15_threeSum.java
@@ -0,0 +1,85 @@
+package com.warship.leedcode.shuzu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ * 你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,1,1]
+ * 输出：[]
+ * 解释：唯一可能的三元组和不为 0 。
+ * <p>
+ * 示例 3：
+ * 输入：nums = [0,0,0]
+ * 输出：[[0,0,0]]
+ * 解释：唯一可能的三元组和为 0 。
+ * <p>
+ * 提示：
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ */
+public class _15_threeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length - 2) {
+            if (nums[i] > 0) {
+                break;
+            }
+            int iNum = nums[i];
+            int x = i + 1;
+            int y = nums.length - 1;
+            while (x < y) {
+                int sum = iNum + nums[x] + nums[y];
+                if (sum == 0) {
+                    List<Integer> tmpAns = new ArrayList<>();
+                    tmpAns.add(iNum);
+                    tmpAns.add(nums[x]);
+                    tmpAns.add(nums[y]);
+                    ans.add(tmpAns);
+                    x = rightMove(nums, x);
+                    y = leftMove(nums, y);
+                } else if (sum > 0) {
+                    y = leftMove(nums, y);
+                } else {
+                    x = rightMove(nums, x);
+                }
+            }
+            i = rightMove(nums, i);
+        }
+        return ans;
+    }
+
+    public int leftMove(int[] nums, int index) {
+        int num = nums[index];
+        while (index > 0 && nums[index] == num) {
+            index--;
+        }
+        return index;
+    }
+
+    public int rightMove(int[] nums, int index) {
+        int num = nums[index];
+        while (index < nums.length && nums[index] == num) {
+            index++;
+        }
+        return index;
+    }
+}
