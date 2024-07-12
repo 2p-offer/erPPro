f04f5cf5c1305ad26a9cffcee46274aed6284aa9
Erp <erp@163.com>
2024-07-12 11:17:06 +0800

    leedCode算法题:除自身以外数组的乘积

---
+++ src/main/java/com/warship/leedcode/shuzu/_238_productExceptSelf.java
@@ -0,0 +1,51 @@
+package com.warship.leedcode.shuzu;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+ * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+ * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * <p>
+ * 示例 1:
+ * 输入: nums = [1,2,3,4]
+ * [1,1,2,6,24]
+ * [24,24,12,4,1]
+ * 输出: [24,12,8,6]
+ * <p>
+ * 示例 2:
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
+ * <p>
+ * 提示：
+ * 2 <= nums.length <= 105
+ * -30 <= nums[i] <= 30
+ * 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内
+ * 进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组 不被视为 额外空间。）
+ */
+public class _238_productExceptSelf {
+
+    public static void main(String[] args) {
+        _238_productExceptSelf clazz = new _238_productExceptSelf();
+        clazz.productExceptSelf(new int[]{1, 2, 3, 4});
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] front = new int[nums.length + 1];
+        int[] end = new int[nums.length + 1];
+        front[0] = 1;
+        end[nums.length] = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            front[i + 1] = front[i] * nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            end[i] = nums[i] * end[i + 1];
+        }
+        int[] answer = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = front[i] * end[i + 1];
+        }
+        return answer;
+    }
+}
